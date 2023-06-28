574ace113331383349c6196e6f278ac999cf36ed
Joel Elrod <joelelrod246@gmail.com>
2023-06-28 14:10:37 -0400

    First version
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

--- lc_single_number/Program.cs
+++ lc_single_number/Program.cs
@@ -5,13 +5,33 @@ Given a non-empty array of integers nums, every element appears twice except for
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 */
 
-int[] testNums = { 4, 1, 2, 1, 2 };
+int[] testNums = { 1, 2, 2, 1, 3 };
 Console.WriteLine(Solution.SingleNumber(testNums));
 
 public class Solution
 {
     static public int SingleNumber(int[] nums)
     {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int element = nums[i];
+            if (nums[i] != 0)
+            {
+                nums[i] = 0;
+            }
+            else
+            {
+                nums[i] = 1;
+            }
 
+            if (!nums.Contains(element))
+            {
+                return element;
+            }
+
+            nums[i] = element;
+        }
+
+        return nums[0]; // if unable to find single number, return first element
     }
 }
